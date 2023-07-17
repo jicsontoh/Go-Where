75824c6975a106eb62adf9b0a17eed2013371812
jicsontoh <jicsontoh@gmail.com>
2023-07-18 00:24:00 +0800

    Add Update Place Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Users from "./user/pages/Users";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
 import NewPlace from "./places/pages/NewPlace";
+import UpdatePlace from "./places/pages/UpdatePlace";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<Users />} />
           <Route path="/:userId/places" element={<UserPlaces />}></Route>
           <Route path="/places/new" element={<NewPlace />}></Route>
+          <Route path="/places/:placeId" element={<UpdatePlace />} />
           <Route path="/error404" element={<h1>No such path</h1>} />
           <Route path="*" element={<Navigate replace to="/error404" />} />
         </Routes>
---
+++ src/places/pages/UpdatePlace.js
@@ -0,0 +1,119 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import { useForm } from "../../shared/hooks/form-hook";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+
+import "./PlaceForm.css";
+
+const UpdatePlace = (props) => {
+  const placeId = useParams().placeId;
+
+  const places = [
+    {
+      id: "p1",
+      title: "Marina Bay Sands",
+      description:
+        "Marina Bay Sands is a destination for those who appreciate luxury. An integrated resort notable for transforming Singapore’s city skyline, it comprises three 55-storey towers of extravagant hotel rooms and luxury suites with personal butler services.",
+      img: "",
+      address: "10 Bayfront Avenue, Singapore 018956",
+      coordinates: {
+        lat: 1.2838,
+        lng: 103.8591,
+      },
+      creatorId: "u1",
+    },
+    {
+      id: "p2",
+      title: "Singapore Flyer",
+      description:
+        "A giant observation wheel that stands amidst the skyscrapers in the Singapore skyline, the Singapore Flyer is the go-to attraction for the most magnificent views of our city.",
+      img: "",
+      address: "30 Raffles Avenue, Singapore 039803",
+      coordinates: {
+        lat: 1.2893,
+        lng: 103.8631,
+      },
+      creatorId: "u2",
+    },
+  ];
+
+  const foundPlaces = places.find((p) => p.id === placeId);
+
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: foundPlaces.title,
+        isValid: true,
+      },
+      address: {
+        value: foundPlaces.address,
+        isValid: true,
+      },
+      description: {
+        value: foundPlaces.description,
+        isValid: true,
+      },
+    },
+    true
+  );
+
+  if (!foundPlaces) {
+    return (
+      <div className="center">
+        <h2>Cannot find place!</h2>
+      </div>
+    );
+  }
+
+  const placeUpdateSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
+  return (
+    <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+        onInput={inputHandler}
+        initialValue={formState.inputs.title.value}
+        initialValid={formState.inputs.title.isValid}
+      />
+      <Input
+        id="address"
+        element="input"
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address"
+        onInput={inputHandler}
+        initialValue={formState.inputs.address.value}
+        initialValid={formState.inputs.address.isValid}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (min. 5 characters)"
+        onInput={inputHandler}
+        initialValue={formState.inputs.description.value}
+        initialValid={formState.inputs.description.isValid}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        Update Place
+      </Button>
+    </form>
+  );
+};
+
+export default UpdatePlace;
