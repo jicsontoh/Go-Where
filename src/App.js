24d11e91c3dc8a86a32299282f50997efd1dff10
jicsontoh <jicsontoh@gmail.com>
2023-07-19 00:23:04 +0800

    Add sign-in context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Users from "./user/pages/Users";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
@@ -6,23 +6,59 @@ import UserPlaces from "./places/pages/UserPlaces";
 import NewPlace from "./places/pages/NewPlace";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import Auth from "./user/pages/Auth";
+import { AuthContext } from "./shared/context/auth-context";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <React.Fragment>
+        <Route path="/" element={<Users />} />
+        <Route path="/:userId/places" element={<UserPlaces />}></Route>
+        <Route path="/places/new" element={<NewPlace />}></Route>
+        <Route path="/places/:placeId" element={<UpdatePlace />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route
+          path="/error404"
+          element={<h1 className="center">No such path</h1>}
+        />
+        <Route path="*" element={<Navigate replace to="/error404" />} />
+      </React.Fragment>
+    );
+  } else {
+    routes = (
+      <React.Fragment>
+        <Route path="/" element={<Users />} />
+        <Route path="/:userId/places" element={<UserPlaces />}></Route>
+
+        <Route path="/auth" element={<Auth />} />
+        <Route path="*" element={<Navigate replace to="/auth" />} />
+      </React.Fragment>
+    );
+  }
+
   return (
-    <BrowserRouter>
-      <MainNavigation />
-      <main>
-        <Routes>
-          <Route path="/" element={<Users />} />
-          <Route path="/:userId/places" element={<UserPlaces />}></Route>
-          <Route path="/places/new" element={<NewPlace />}></Route>
-          <Route path="/places/:placeId" element={<UpdatePlace />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/error404" element={<h1>No such path</h1>} />
-          <Route path="*" element={<Navigate replace to="/error404" />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <BrowserRouter>
+        <MainNavigation />
+        <main>
+          <Routes>{routes}</Routes>
+        </main>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 };
 
