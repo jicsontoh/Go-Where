ca86b1d08a39d6bb0fe23dea1a53a64922d334e2
jicsontoh <jicsontoh@gmail.com>
2023-07-18 23:42:12 +0800

    Add Authentication Route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
 import NewPlace from "./places/pages/NewPlace";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/:userId/places" element={<UserPlaces />}></Route>
           <Route path="/places/new" element={<NewPlace />}></Route>
           <Route path="/places/:placeId" element={<UpdatePlace />} />
+          <Route path="/auth" element={<Auth />} />
           <Route path="/error404" element={<h1>No such path</h1>} />
           <Route path="*" element={<Navigate replace to="/error404" />} />
         </Routes>
